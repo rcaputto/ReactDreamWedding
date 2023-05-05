1d8f97cbf2cf1f1f64a7f8f8f155d6b20aa67b78
rcaputto <rgcaputto@gmail.com>
2023-05-05 18:14:05 -0300

    first deploy

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,69 +1,64 @@
-import react from 'react';
+import { Link } from 'react-router-dom'
 import logo from '../public/images/logo.png';
 import imageCarrousel1 from '../public/images/random/pexels-agung-pandit-wiguna-2970287.jpg';
 import imageCarrousel2 from '../public/images/random/pexels-danielle-reese-2496344.jpg';
 import imageCarrousel3 from '../public/images/random/pexels-dmitry-zvolskiy-1676123.jpg';
 import imageCarrousel4 from '../public/images/random/pexels-emir-kaan-okutan-2403568.jpg';
 import '../public/css/home.css'
-import backgroundI from '../public/images/pexels-asad-photo-maldives-1024993.jpg'
-function Home (){
-    return(
-        <>
-        <body>
-        <div/>
+
+function Home() {
+  return (
+    <>
+      <body>
+        <div />
         <div className="top">
-        <div className="logotipo">
-        <img src={logo} className="logo"  alt="logo"/>
-        
-          <nav>
-          
-          
-          <div className="hamburger">
-            <span className="lines"></span>
-            <span className="lines"></span>
-            <span className="lines"></span>
+          <div className="logotipo">
+            <img src={logo} className="logo" alt="logo" />
+
+            <nav>
+              <div className="hamburger">
+                <span className="lines"></span>
+                <span className="lines"></span>
+                <span className="lines"></span>
+              </div>
+
+
+              <ul id="nav-links">
+                <li><Link to="#" className="links">Inicio</Link></li>
+                <li><Link to="#" className="links">Galeria</Link></li>
+                <li> <a href='#servicios'> Servicios</a></li>
+                <li><Link to="#" className="links">Nosotras</Link></li>
+                <li><Link to="#" className="links">Blog</Link></li>
+                <li><Link to="#" className="links">Contacto</Link></li>
+                <div className="linea"></div>
+              </ul>
+            </nav>
           </div>
-          
-          
-          <ul id="nav-links">
-            <li><a href="#" className="links">Inicio</a></li>
-            <li><a href="#" className="links">Galeria</a></li>
-            <li><a href="#servicios" className="links">Servicios</a></li>
-            <li><a href="#" className="links">Nosotras</a></li>
-            <li><a href="#" className="links">Blog</a></li>
-            <li><a href="#" className="links">Contacto</a></li>
-            <div className="linea"></div>
-          </ul>
-          </nav>
-          </div>
-          </div>
-      
-         
-        
+        </div>
+
+
+
         <h1 className="slogan">
           Tu boda, tu estilo, nuestra pasión!
         </h1>
         <h2 className="sloganCopy">
-          Creamos experiencias que duran toda la vida. <br/> 
+          Creamos experiencias que duran toda la vida. <br />
           Sin estrés ni preocupaciones...
         </h2>
-        
       
-          <br/>
-      
-      
+    <br />
       <h1 className="carrouselTitle" id="servicios">Creamos momentos inolvidables</h1>
-      <div className="carrouselImg">
-        <article className="carrousel">
-          <img src={imageCarrousel1} alt="1"/>
-          <img src={imageCarrousel2} alt="2"/>
-          <img src={imageCarrousel3} alt="3"/>
-          <img src={imageCarrousel4} alt="4"/>
-        </article>
-      </div>
+        <div className="carrouselImg">
+          <article className="carrousel">
+            <img src={imageCarrousel1} alt="1" />
+            <img src={imageCarrousel2} alt="2" />
+            <img src={imageCarrousel3} alt="3" />
+            <img src={imageCarrousel4} alt="4" />
+          </article>
+        </div>
       </body>
-      </>
-    )
+    </>
+  )
 }
 
 export default Home
